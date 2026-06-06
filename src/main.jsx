b6f1b152ef62035c2f696c5f80ea9fef0d374c8b
lucasdevsilva26 <lucasferreirasilvaadry@gmail.com>
2026-06-06 20:27:13 -0300

    update vercel analytics version

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router";
-import { Analytics } from "@vercel/analytics/next";
+import { Analytics } from "@vercel/analytics/react";
 import Portfolio from "./pages/Portfolio";
 
 const router = createBrowserRouter([
