c141d646523c8c68e0e29d56152f28233ef4f9d1
lucasdevsilva26 <lucasferreirasilvaadry@gmail.com>
2026-06-06 20:02:31 -0300

    update cards desing and layout

--- src/components/ProjectList.jsx
+++ src/components/ProjectList.jsx
@@ -5,11 +5,11 @@ function ProjectList({ projects, setLoading }) {
     <>
       <div
         className={
-          ` flex flex-col gap-2 py-4 bg-linear-0 from-gray-900/75 to-gray-800/75 text-center text-xl relative ` +
+          ` flex flex-col gap-2 w-full py-4 bg-linear-0 from-gray-900/75 to-gray-800/75 text-center text-xl relative ` +
           " gradientBorder before:py-1 "
         }
       >
-        <h1 className={` z- text-4xl font-bold `}>MEUS PROJETOS</h1>
+        <h1 className={` text-4xl font-bold `}>MEUS PROJETOS</h1>
         <span className={` flex items-center justify-center gap-1 px-5 `}>
           <i className={` fas fa-triangle-exclamation text-amber-400 `}></i>
           Clique no projeto para ser redirecionado
@@ -25,7 +25,7 @@ function ProjectList({ projects, setLoading }) {
             <article
               key={crypto.randomUUID()}
               className={
-                ` flex flex-col w-full max-w-100 min-h-87 p-3 rounded-2xl relative scale-95 hover:scale-100 hover:brightness-115 duration-500 overflow-hidden bg-linear-45 from-gray-700 to-gray-900 ` +
+                ` flex flex-col w-full max-w-100 h-100 p-3 rounded-2xl relative scale-95 hover:scale-100 hover:brightness-115 duration-500 overflow-hidden bg-linear-45 from-gray-700 to-gray-900 ` +
                 " gradientBorder before:p-1.5 "
               }
               onClick={() => {
@@ -34,18 +34,23 @@ function ProjectList({ projects, setLoading }) {
               }}
             >
               <div
-                className={` flex gap-2 h-full p-2 bg-gray-700 rounded-xl cursor-pointer `}
+                className={` flex flex-col gap-2 h-full p-2 bg-gray-700 rounded-xl cursor-pointer `}
               >
                 <div
-                  className={` flex-1 flex flex-col items-center  justify-center z-2 `}
+                  className={` flex-1 flex items-center justify-center gap-2 `}
                 >
                   <i
-                    className={` flex ${project.icon} py-6 bg-gray-600 ${project.color} text-8xl rounded-2xl `}
+                    className={` flex ${project.icon} ${project.color} text-3xl rounded-2xl `}
+                  ></i>
+                  <h1 className={` text-4xl font-bold `}>{project.title}</h1>
+                  <i
+                    className={` flex ${project.icon} ${project.color} text-3xl rounded-2xl `}
                   ></i>
                 </div>
 
-                <div className={` flex-2 flex flex-col text-justify p-2 z-2 `}>
-                  <h1 className={` text-4xl font-bold `}>{project.title}</h1>
+                <div
+                  className={` flex-2 flex text-justify p-2 wrap-break-word bg-gray-800 rounded-xl overflow-hidden `}
+                >
                   <p className={` pl-3 pt-2 w-9/10 text-wrap `}>
                     {project.description}
                   </p>
